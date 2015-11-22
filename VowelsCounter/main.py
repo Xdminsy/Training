b2501b0a5569b1c41e3e4eea719395fcfc6d3645
xdminsy <xsy2293419066@gmail.com>
2015-11-22 11:33:42 +0800

    finish VowelsCounter

---
+++ VowelsCounter/main.py
@@ -0,0 +1,13 @@
+def getTimes(times):
+    if times < 0:
+        raise AttributeError('The attribute `times` cannot be less then 0')
+    elif times == 1:
+        return 'once'
+    elif times == 2:
+        return 'twice'
+    else:
+        return '%i times' % times
+while True:
+    line = input('>>> ')
+    for i in 'aeiou':
+        print("%c :" % i, getTimes(line.count(i)))
